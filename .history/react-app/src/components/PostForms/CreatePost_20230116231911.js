69fc6534fc8bfa1c2ee53727579776b9dc5959ab
Risclover <sara091592@gmail.com>
2023-01-16 23:28:15 -0800

    Bug fixes - 'create a post' form, deleting a post with comments

---
+++ .history/react-app/src/components/PostForms/CreatePost_20230116231911.js
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addPost, getPosts } from "../../store/posts";
+import { useHistory } from "react-router-dom";
+import { Modal } from "../../context/Modal";
+import "./PostForm.css";
+import DiscardPost from "../Modals/DiscardPost";
+import { getCommunities } from "../../store/communities";
+
+export default function CreatePost() {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [community_id, setcommunity_id] = useState();
+  const [disabled, setDisabled] = useState(false);
+  const [errors, setErrors] = useState([]);
+  const [showDiscardModal, setShowDiscardModal] = useState(false);
+  const history = useHistory();
+  const communities = useSelector((state) => Object.values(state.communities));
+
+  useEffect(() => {
+    dispatch(getPosts());
+    dispatch(getCommunities());
+    // REMEMBER: If no community, also set it to disabled.
+    if (
+      title.length === 0 ||
+      content.length === 0 ||
+      community_id === undefined
+    ) {
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+  }, [dispatch, title, content]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = dispatch(addPost({ title, content, community_id }));
+    if (data.errors) {
+      setErrors(data.errors);
+    } else {
+      setTitle("");
+      setContent("");
+      setcommunity_id();
+      history.push("/posts");
+    }
+  };
+
+  return (
+    <div className="create-post-form-container">
+      <form className="create-post-form" onSubmit={handleSubmit}>
+        <div className="create-post-header">Create a post</div>
+        <div className="create-post-choose-community">
+          <select
+            defaultValue="default"
+            onChange={(e) => setcommunity_id(e.target.value)}
+            value={community_id}
+            className="choose-community-dropdown"
+          >
+            <option value="default" disabled>
+              Choose a community
+            </option>
+            {communities.length > 0 &&
+              communities.map((community) => (
+                <option value={community.id}>c/{community.name}</option>
+              ))}
+          </select>
+        </div>
+        <div className="create-post-content">
+          <div className="create-post-form-input">
+            <textarea
+              placeholder="Title"
+              className="create-post-input title-input"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              maxLength={300}
+            ></textarea>
+            <div className="create-post-form-title-length">
+              <span className="create-post-title-length">
+                {title.length}/300
+              </span>
+            </div>
+          </div>
+          <div className="create-post-form-input">
+            <textarea
+              onChange={(e) => setContent(e.target.value)}
+              className="create-post-input content-input"
+              value={content}
+              maxLength={40000}
+              placeholder="Text"
+            ></textarea>
+          </div>
+          <div className="create-post-form-errors">
+            {errors && errors.length > 0
+              ? errors.map((error) => <div>{error}</div>)
+              : ""}
+          </div>
+          <div className="create-post-form-buttons">
+            <button
+              className="create-post-form-cancel"
+              onClick={(e) => {
+                e.preventDefault();
+                content.length > 0
+                  ? setShowDiscardModal(true)
+                  : history.push("/posts");
+              }}
+            >
+              Cancel
+            </button>
+            {showDiscardModal && (
+              <Modal
+                title="Discard post?"
+                onClose={() => setShowDiscardModal(false)}
+              >
+                <DiscardPost
+                  setShowDiscardModal={setShowDiscardModal}
+                  showDiscardModal={showDiscardModal}
+                />
+              </Modal>
+            )}
+            <button
+              disabled={disabled}
+              type="submit"
+              className="create-post-form-submit"
+            >
+              Post
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ .history/react-app/src/components/Posts/SinglePost/SinglePostPage_20230116215547.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import SinglePost from "./SinglePost";
+import { useDispatch, useSelector } from "react-redux";
+import Comments from "../../Comments/Comments";
+import { getComments } from "../../../store/comments";
+import { getPosts } from "../../../store/posts";
+import { getSinglePost } from "../../../store/one_post";
+import { getSingleCommunity } from "../../../store/one_community";
+import moment from "moment";
+import Cake from "../../../images/piece4.png";
+
+export default function SinglePostPage({ setShowLoginForm }) {
+  const { postId } = useParams();
+  const [isPage, setIsPage] = useState(true);
+  const dispatch = useDispatch();
+  const post = useSelector((state) => state.posts[postId]);
+  const comments = useSelector((state) => Object.values(state.comments));
+  const posts = useSelector((state) => state.posts);
+  const community = useSelector((state) => state.singleCommunity);
+
+  useEffect(() => {
+    dispatch(getComments(postId));
+    dispatch(getPosts());
+    dispatch(getSinglePost(postId));
+    dispatch(getSingleCommunity(posts[postId]?.communityId));
+  }, [dispatch, postId]);
+
+  // comments.sort((a, b) => {
+  //   let commentA = new Date(a.createdAt).getTime();
+  //   let commentB = new Date(b.createdAt).getTime();
+  //   return commentB - commentA;
+  // });
+
+  if (!comments || !posts || !postId) return null;
+  return (
+    <div className="single-post-page">
+      <div className="single-post-left-col">
+        {/* <NavLink to={`/posts/${+postId}`}> */}
+        <SinglePost id={+postId} isPage={isPage} />
+        {/* </NavLink> */}
+        <Comments postId={+postId} setShowLoginForm={setShowLoginForm} />
+      </div>
+      <div className="single-post-right-col">
+        <div className="single-post-community-info">
+          <div className="single-post-community-name">
+            c/{post.postCommunity?.name}
+          </div>
+          <div className="single-post-community-description">
+            {post.postCommunity?.description}
+          </div>
+          <div className="single-post-community-date">
+            <img src={Cake} className="single-post-community-cake" />{" "}
+            {moment(post.postCommunity?.createdAt).format("MMM DD, YYYY")}
+          </div>
+        </div>
+        <div className="single-post-community-rules"></div>
+      </div>
+    </div>
+  );
+}
