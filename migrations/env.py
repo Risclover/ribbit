4e1372616418da0e453c5f99631029976792edc1
Risclover <sara091592@gmail.com>
2023-01-27 05:53:48 -0800

    Heroku

--- migrations/env.py
+++ migrations/env.py
@@ -8,10 +8,6 @@ from sqlalchemy import pool
 
 from alembic import context
 
-import os
-environment = os.getenv("FLASK_ENV")
-
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -89,15 +85,10 @@ def run_migrations_online():
             process_revision_directives=process_revision_directives,
             **current_app.extensions['migrate'].configure_args
         )
-        # # Create a schema (only in production)
-        # if environment == "production":
-        #     connection.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA}")
 
-        # # Set search path to your schema (only in production)
-        # with context.begin_transaction():
-        #     if environment == "production":
-        #         context.execute(f"SET search_path TO {SCHEMA}")
-        #     context.run_migrations()
+        with context.begin_transaction():
+            context.run_migrations()
+
 
 if context.is_offline_mode():
     run_migrations_offline()
