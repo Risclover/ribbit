06292d945084594772c596a8b9aaea19488af63c
Risclover <sara091592@gmail.com>
2024-10-18 18:35:17 -0700

    Create custom hook for SignUpFormSecondPage and extract logic

--- react-app/src/features/Auth/components/SignUpForm/SignUpFormSecondPage.js
+++ react-app/src/features/Auth/components/SignUpForm/SignUpFormSecondPage.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { AuthFormInput } from "../AuthFormInput";
-import { useUsernameTaken } from "../../hooks/useUsernameTaken.hook";
 import {
   validatePassword,
   validateUsername,
 } from "../../utils/signupFormValidation";
+import useSignUpFormSecondPage from "features/Auth/hooks/useSignUpFormSecondPage";
 
 export function SignUpFormSecondPage({
   setDisabled,
@@ -13,40 +13,8 @@ export function SignUpFormSecondPage({
   password,
   setPassword,
 }) {
-  const [usernameErrors, setUsernameErrors] = useState([]);
-  const [passwordErrors, setPasswordErrors] = useState([]);
-  const usernameTaken = useUsernameTaken(username);
-
-  const inputProps = (name, value, setValue, errors) => ({
-    type: name,
-    name,
-    inputValue: value,
-    setInputValue: setValue,
-    errors,
-    setErrors: name === "username" ? setUsernameErrors : setPasswordErrors,
-    maxLength: name === "username" ? 20 : 255,
-    label: name.charAt(0).toUpperCase() + name.slice(1),
-    autoCompleteStatus: name === "password" ? "new-password" : "off",
-    testId: name.charAt(0).toUpperCase() + name.slice(1),
-  });
-
-  useEffect(() => {
-    const usernameErrors = validateUsername(username, usernameTaken);
-    const passwordErrors = validatePassword(password);
-    setDisabled(
-      username === "" ||
-        usernameErrors.length > 0 ||
-        password === "" ||
-        passwordErrors.length > 0
-    );
-  }, [
-    username,
-    setDisabled,
-    setUsernameErrors,
-    password,
-    setPasswordErrors,
-    usernameTaken,
-  ]);
+  const { inputProps, usernameErrors, passwordErrors, usernameTaken } =
+    useSignUpFormSecondPage({ setDisabled, username, password });
 
   return (
     <div>
---
+++ react-app/src/features/Auth/hooks/useSignUpFormSecondPage.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { useUsernameTaken } from "./useUsernameTaken.hook";
+import { validatePassword, validateUsername } from "../utils";
+
+export default function useSignUpFormSecondPage({
+  setDisabled,
+  username,
+  password,
+}) {
+  const [usernameErrors, setUsernameErrors] = useState([]);
+  const [passwordErrors, setPasswordErrors] = useState([]);
+  const usernameTaken = useUsernameTaken(username);
+
+  const inputProps = (name, value, setValue, errors) => ({
+    type: name,
+    name,
+    inputValue: value,
+    setInputValue: setValue,
+    errors,
+    setErrors: name === "username" ? setUsernameErrors : setPasswordErrors,
+    maxLength: name === "username" ? 20 : 255,
+    label: name.charAt(0).toUpperCase() + name.slice(1),
+    autoCompleteStatus: name === "password" ? "new-password" : "off",
+    testId: name.charAt(0).toUpperCase() + name.slice(1),
+  });
+
+  useEffect(() => {
+    const usernameErrors = validateUsername(username, usernameTaken);
+    const passwordErrors = validatePassword(password);
+    setDisabled(
+      username === "" ||
+        usernameErrors.length > 0 ||
+        password === "" ||
+        passwordErrors.length > 0
+    );
+  }, [
+    username,
+    setDisabled,
+    setUsernameErrors,
+    password,
+    setPasswordErrors,
+    usernameTaken,
+  ]);
+
+  return { usernameErrors, passwordErrors, inputProps, usernameTaken };
+}
