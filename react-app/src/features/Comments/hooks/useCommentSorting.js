38d4a57eec3117548877a92f6ef10777f4c0ec5e
Risclover <sara091592@gmail.com>
2025-04-14 22:46:53 -0700

    move SORT_OPTIONS to constants

--- react-app/src/features/Comments/hooks/useCommentSorting.js
+++ react-app/src/features/Comments/hooks/useCommentSorting.js
@@ -1,13 +1,11 @@
 import { useRef, useState } from "react";
-
+import { SORT_OPTIONS } from "../data/constants";
 /**
  * Logic for CommentSorting component
  */
 export function useCommentSorting({ setSortType }) {
   const [showDropdown, setShowDropdown] = useState(false);
   const wrapperRef = useRef(null);
-
-  const SORT_OPTIONS = ["Best", "Top", "New", "Old"];
 
   const toggleDropdown = () => setShowDropdown((prev) => !prev);
 
